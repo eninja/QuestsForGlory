5411c45dc8fad10b8f021ebf06ba58580c89095c
eNinjaPL <info@eninja.pl>
2018-03-18 16:19:15 +0100

    remove comment

--- src/main/java/pl/eninja/firstDuplicate.java
+++ src/main/java/pl/eninja/firstDuplicate.java
@@ -15,25 +15,7 @@ class firstDuplicate {
                 }
             }
         }
-
         return result;
-
     }
 
 }
-
-
-// int counter = 0;
-//    int temp = -1;
-//    for(int i = 0; i < a.length; i++){
-//        for(int j = i + 1; j < a.length; j++){
-//            if(a[i] == a[j] ){
-//                counter++;
-//                    if(counter <= a[j]){
-//                        temp = a[i];
-//                    }
-//            }else
-//                temp = temp;
-//        }
-//    }
-//    return temp;}
