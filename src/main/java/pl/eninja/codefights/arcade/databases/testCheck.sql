61ab0f9ebee8aae1c9e136499c51c1d84d6afec1
eNinjaPL <info@eninja.pl>
2018-04-15 14:51:25 +0200

    expressionsVerification, securityBreach, suspectsInvestigation2, testCheck solution

---
+++ src/main/java/pl/eninja/codefights/arcade/databases/testCheck.sql
@@ -0,0 +1,6 @@
+CREATE PROCEDURE testCheck()
+BEGIN
+    SELECT id, IF ( correct_answer = given_answer, 'correct', IF(given_answer IS NULL, 'no answer', 'incorrect') ) AS checks
+    FROM answers
+    ORDER BY id;
+END
