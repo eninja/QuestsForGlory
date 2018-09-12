5d6aa491c99783284f5b7d00807ff13bb825ba9b
eninja <info@eninja.pl>
2018-09-12 13:33:04 +0200

    LateRide solution and tests

---
+++ src/main/java/pl/eninja/codefights/arcade/intro/LateRide.java
@@ -0,0 +1,20 @@
+/*One night you go for a ride on your motorcycle. At 00:00 you start your engine, and the built-in timer automatically begins counting the length of your ride, in minutes. Off you go to explore the neighborhood.
+
+When you finally decide to head back, you realize there's a chance the bridges on your route home are up, leaving you stranded! Unfortunately, you don't have your watch on you and don't know what time it is. All you know thanks to the bike's timer is that n minutes have passed since 00:00.
+
+Using the bike's timer, calculate the current time. Return an answer as the sum of digits that the digital timer in the format hh:mm would show.*/
+
+package pl.eninja.codefights.arcade.intro;
+
+class LateRide {
+
+    int lateRide(int n) {
+        int hour = n / 60;
+        int minutes = n % 60;
+        int firstDigitOfHour = hour / 10;
+        int secondDigitOfHour = hour % 10;
+        int firstDigitOfMinutes = minutes / 10;
+        int secondDigitOfMinutes = minutes % 10;
+        return firstDigitOfHour + secondDigitOfHour + firstDigitOfMinutes + secondDigitOfMinutes;
+    }
+}
---
+++ src/test/java/pl/eninja/codefights/arcade/intro/LateRideTest.java
@@ -0,0 +1,35 @@
+package pl.eninja.codefights.arcade.intro;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LateRideTest {
+    private LateRide lateRide = new LateRide();
+
+    @Test
+    public void lateRideTest1() {
+        assertEquals(4, lateRide.lateRide(240));
+    }
+
+    @Test
+    public void lateRideTest2() {
+        assertEquals(14, lateRide.lateRide(808));
+    }
+    @Test
+    public void lateRideTest3() {
+        assertEquals(19, lateRide.lateRide(1439));
+    }
+    @Test
+    public void lateRideTest4() {
+        assertEquals(0, lateRide.lateRide(0));
+    }
+    @Test
+    public void lateRideTest5() {
+        assertEquals(5, lateRide.lateRide(23));
+    }
+    @Test
+    public void lateRideTest6() {
+        assertEquals(8, lateRide.lateRide(8));
+    }
+}
